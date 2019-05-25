3715f625aca490b3c6bf3e66ffbc05640d80899c
npatel711 <44736598+npatel711@users.noreply.github.com>
2019-05-24 21:03:28 -0500

    Add files via upload

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,12 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-var button = d3.select("filter-btn");
-var inputField1 = d3.select("datetime");
-var inputField2 = d3.select("city");
+// Select the submit button
+var submit = d3.select("#submit");
 var tbody = d3.select("tbody");
-var resetbtn = d3.select("reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments"]
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
 
 var populate = (dataInput) => {
 
@@ -17,44 +16,26 @@ var populate = (dataInput) => {
     )
   });
 }
-
-//Populate table
 populate(data);
 
-// Filter by attribute
-button.on("click", () => {
+submit.on("click", function () {
   d3.event.preventDefault();
-  var inputDate = inputField1.property("value").trim();
-  var inputCity = inputField2.property("value").toLowerCase().trim();
-  // Filter by field matching input value
-  var filterDate = data.filter(data => data.datetime === inputDate);
-  console.log(filterDate)
-  var filterCity = data.filter(data => data.city === inputCity);
-  console.log(filterCity)
-  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
-  console.log(filterData)
 
-  // Add filtered sighting to table
-  tbody.html("");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  var filteredData = data.filter(info => info.datetime === inputValue);
+  console.log(filteredData);
 
-  let response = {
-    filterData, filterCity, filterDate
-  }
-
-  if (response.filterData.length !== 0) {
-    populate(filterData);
-  }
-    else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-      populate(filterCity) || populate(filterDate);
-  
-    }
-    else {
-      tbody.append("tr").append("td").text("No results found!"); 
-    }
-})
+  filteredData.forEach(ufo_sightings => {
+    var row = tbody.append("tr");
+    columns.forEach(column => row.append("td").text(ufo_sightings[column])
+    )
+  });
 
 resetbtn.on("click", () => {
-  tbody.html("");
-  populate(data)
-  console.log("Table reset")
+   tbody.html("");
+   populate(data)
+   console.log("Table reset")
+  });
 })
